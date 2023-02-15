59c4436bd9ee3960f9f7d264506fba1ad03f71e3
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-02-15 01:06:33 +0100

    Created user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const User = new mongoose.Schema(
 	{
-		name: { type: String, required: true },
+		firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
-		quote: { type: String },
 	},
 	{ collection: 'user-data' }
 )
